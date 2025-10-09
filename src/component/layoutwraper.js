50132290656b5de56e4ee5d1cc5f8c6684bba6c4
iftikhar ali <ifti.hazara205@gmail.com>
2025-10-09 10:30:27 +0500

    near to complete

---
+++ src/component/layoutwraper.js
@@ -0,0 +1,55 @@
+"use client"
+import React from 'react'
+import { Github, Linkedin, Instagram } from "lucide-react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+const layoutwraper = () => {
+  return (
+     <div className="hidden [@media(min-width:1400px)]:flex z-51 absolute left-0 top-0 flex-col justify-center pl-4 items-center">
+            <motion.div
+              className="h-[160px] w-[2px] bg-[var(--accent)] rounded-full origin-top"
+              initial={{ scaleY: 0 }}
+              whileInView={{ scaleY: 1 }}
+              transition={{ duration: 1, ease: "easeOut" }}
+              viewport={{ once: true, amount: 0.3 }}
+            />
+
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.7 }}
+              className="flex flex-col justify-center items-center gap-6 pt-2"
+            >
+              {[
+                {
+                  href: "https://github.com/",
+                  Icon: Github,
+                },
+                {
+                  href: "https://linkedin.com/",
+                  Icon: Linkedin,
+                },
+                {
+                  href: "https://instagram.com/",
+                  Icon: Instagram,
+                },
+              ].map(({ href, Icon }, i) => (
+                <Link
+                  key={i}
+                  href={href}
+                  target="_blank"
+                  className="relative group transition-all"
+                >
+                  <Icon
+                    size={24}
+                    className="text-[var(--text)] group-hover:text-[var(--accent)] transition-all"
+                  />
+                  <span className="absolute inset-0 scale-0 group-hover:scale-100 rounded-full bg-[var(--accent)]/10 transition-transform duration-300"></span>
+                </Link>
+              ))}
+            </motion.div>
+          </div>
+  )
+}
+
+export default layoutwraper
